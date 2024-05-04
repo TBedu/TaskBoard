351ceb6d55731a730183568388ff51b526f9e76a
TBedu <155792714+TBedu@users.noreply.github.com>
2024-05-04 08:40:18 +0800

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+function acceptTouching() {
+    document.getElementById("EULA_accept").remove();
+}
+var photo = 1;
+function changePhoto() {
+    console.log("It worked, it worked!")
+    photo++;
+    var place = "/assets/img/background/" + photo + ".jpg";
+    document.body.style.backgroundImage = "url('" + place + "')";
+    console.log(place);
+    if (photo == 10) photo = 0;
+}
